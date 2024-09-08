5440747e256c0924e4601ba79ba9a1933272cf97
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-09-08 15:11:43 +0500

    chore: working with forms, added csrf token, post method and retreive data from database

--- chaiaurDjango/chai/views.py
+++ chaiaurDjango/chai/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import ChaiVariety
+from .models import ChaiVariety,Store
 from django.shortcuts import get_object_or_404
+from .forms import ChaiVarietyForm
 # Create your views here.
 
 def all_chai(request):
@@ -12,4 +13,12 @@ def chai_detail(request, chai_id):
     return render(request, 'chai/chai_detail.html',{'chai': chai})
 
 def chai_store_view(request):
-    return render(request, "chai/chai_stores.html")
+    stores = None
+    if request.method == "POST":
+        form = ChaiVarietyForm(request.POST)
+        if form.is_valid():
+            chai_variety = form.cleaned_data['chai_variety']
+            stores = Store.objects.filter(ChaiVariety = chai_variety)
+    else:
+        form = ChaiVarietyForm()
+    return render(request, "chai/chai_stores.html", {'form': form, 'stores': stores})
